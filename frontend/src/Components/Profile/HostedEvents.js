6a793c6a7899590a336cdd39aad162481a3f5185
Arpa Imran <aarpa128@gmail.com>
2019-11-25 17:15:18 -0800

    Working components to render 2 categories of events related to user.

--- frontend/src/Components/Profile/HostedEvents.js
+++ frontend/src/Components/Profile/HostedEvents.js
@@ -1,30 +1,52 @@
 import React from 'react';
+import { 
+  Link,
+  // useRouteMatch
+} from "react-router-dom";
 import $ from "jquery";
 
 
 class HostedEvents extends React.Component {
-  // constructor(props) {
-  //   super(props);
-  // //   this.state = { 
-  // //     userBio: {}
-  // //   }
-  // }
+  constructor(props) {
+    super(props);
+    this.state = { hostedEvents: [] }
+  }
 
-  // componentDidMount() {
-  //   let userId = this.props.match.params.userId;
-  //   let apiUrl = `/users/${userId}`
-    
-  //   $.get(apiUrl, response => this.setState({ userBio: response }));
-  // }
+  componentDidMount() {
+    // let apiUrl = `/users/${this.props.match.url}/hosted-events`
+    let apiUrl = this.props.match;
+
+    $.get(apiUrl, response => this.setState({ hostedEvents: response }));
+  }
 
   render() {
-    // let userBio = this.state.userBio;
+    let hostedEvents = this.state.hostedEvents;
 
-    return (
-      <div>
-        Details about the hosted events go here
-      </div>        
-    );
+    let eventsList = [];
+
+    for (let event of hostedEvents) {
+      let redirectUrl = `/events/${event.event_id}`;
+
+      // event is an object, so each attribute is a direct key 
+      eventsList.push(
+        <div>
+          <Link to={redirectUrl}>{event.title}</Link>
+          <p>Start Time and Date: {event.start_on}</p>
+          <p>End Time and Date: {event.end_on}</p>
+        </div>
+      );
+    }
+
+    if (eventsList === []) {
+      return (<p>"No upcoming events hosted by you."</p>)
+    } else {
+      return (
+        <div>
+          {eventsList}
+        </div>        
+      );
+    }
+
   }
 }
 
--- frontend/src/Components/Profile/Invites.js
+++ frontend/src/Components/Profile/Invites.js
@@ -1,30 +1,52 @@
 import React from 'react';
+import { 
+  Link,
+  // useRouteMatch
+} from "react-router-dom";
 import $ from "jquery";
 
 
 class Invites extends React.Component {
-  // constructor(props) {
-  //   super(props);
-  // //   this.state = { 
-  // //     userBio: {}
-  // //   }
-  // }
+  constructor(props) {
+    super(props);
+    this.state = { invitedEvents: [] }
+  }
 
-  // componentDidMount() {
-  //   let userId = this.props.match.params.userId;
-  //   let apiUrl = `/users/${userId}`
-    
-  //   $.get(apiUrl, response => this.setState({ userBio: response }));
-  // }
+  componentDidMount() {
+    // let apiUrl = `/users/${this.props.match.url}/hosted-events`
+    let apiUrl = this.props.match;
+
+    $.get(apiUrl, response => this.setState({ invitedEvents: response }));
+  }
 
   render() {
-    // let userBio = this.state.userBio;
+    let invitedEvents = this.state.invitedEvents;
 
-    return (
-      <div>
-        Details about the invited-to events go here
-      </div>        
-    );
+    let eventsList = [];
+
+    for (let eventObj of invitedEvents) {
+      let redirectUrl = `/events/${eventObj.event.event_id}`;
+
+      // eventObj has a key called 'event' that contains a nested obj
+      eventsList.push(
+        <div>
+          <Link to={redirectUrl}>{eventObj.event.title}</Link>
+          <p>Start Time and Date: {eventObj.event.start_on}</p>
+          <p>End Time and Date: {eventObj.event.end_on}</p>
+        </div>
+      );
+    }
+
+    if (eventsList === []) {
+      return (<p>"No upcoming invites."</p>)
+    } else {
+      return (
+        <div>
+          {eventsList}
+        </div>        
+      );
+    }
+
   }
 }
 
